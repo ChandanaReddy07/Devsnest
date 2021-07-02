a5871d28cb6133e7d79243cebf2a365db5271106
dcr7978933k <b219021@iiit-bh.ac.in>
2021-07-02 07:26:37 -0700

    updated d-11

--- Day11/script.js
+++ Day11/script.js
@@ -1,18 +1,21 @@
 const x= document.querySelector(".slideshow-container")
 console.log(x)
-
-var list=[
+var count=0
+const list=[
     {
         quest:"Who invented the Light Bulb?",
-        options:["Thomas Alva Edison","albert Einstine", "Tesla","Galilio"]
+        options:["Thomas Alva Edison","albert Einstine", "Tesla","Galilio"],
+        correct:"Thomas Alva Edison"
     },
     {
         quest:"Which planet in our solar system is known as the Red Planet?",
-        options:["Mars","Earth","Venus","Saturn"]
+        options:["Mars","Earth","Venus","Saturn"],
+        correct:"Mars"
     },
     {
         quest:"Who invented the Telephone?",
-        options:["Thomas Alva Edison","albert Einstine", "Tesla","Alexander Graham Bell"]
+        options:["Thomas Alva Edison","albert Einstine", "Tesla","Alexander Graham Bell"],
+        correct:"Alexander Graham Bell"
     }
 ]
 
@@ -31,10 +34,15 @@ function quest(i){
     question.appendChild(options)
    
     for(var j=0;j<4;j++)
-   { var option=document.createElement("li")
-   var buttons=document.createElement("button")
-   buttons.className="optionSelected"
+   {
+   var option=document.createElement("li")
+   var buttons=document.createElement("span")
+   buttons.className="optionek"
    buttons.id=j
+   buttons.addEventListener("click",(e)=>{ console.log("yeh"+list[e.target.id])
+       if(list[i].options[e.target.id]===list[i].correct)
+       console.log("yess correct"+ ++count)
+   })
    buttons.textContent=`Option ${j+1}`
    option.innerText=list[i].options[j]
    options.appendChild(option)
@@ -51,17 +59,29 @@ function quest(i){
 
 for(var i=0;i<list.length;i++){
     quest(i)
+    //  var v=document.querySelectorAll(".optionSelected")
+    // console.log("v"+v)
+    // console.log("quesss"+ list[i])
+    
+    // v.forEach(but=>but.addEventListener("click",(e) => {
+
+    //     console.log("quesss"+ list[i])
+        
+    //     if(list[i].quest[e.target.id]===list[i].correct)
+
+    //     console.log("clasnam yess correct"+e.target.id)
+    // }))
 }
 
+   
+ 
+// function checkIt (e) {
 
-//creating buttons
-
-
-
-
-function checkIt(){
-   console.log(this)
-}
+//     console.log("class name"+e.target.className)
+//     // if (e.target.className == 'delete') {
+//     //     const li = e.target.parentElement;
+//     //   list.removeChild(li);
+//     }
 
 
 
